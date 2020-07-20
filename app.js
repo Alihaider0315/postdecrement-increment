959e17e8826e87009028bb4c04f458d1cead6de6
Alihaider0315 <ah712200@gmail.com>
2020-07-20 20:10:06 +0500

    Now Ready To Commit

---
+++ app.js
@@ -0,0 +1,42 @@
+// alert("Hello World");
+
+// var a = 5;
+// var b= 10;
+// var c= a+b;
+// alert(c);
+//
+
+// var a = 10;
+// var b = "Ali Haider";
+// var c= a + b;
+// alert(c);
+//
+//
+// var a = 10;
+// var b = 20;
+// var c = a + b;
+// console.log (c);
+//
+// var a = "Ali";
+// var b = "Haider";
+// var c = a+b;
+// console.log (c);
+
+// var a = 10;
+// var b = 15;
+// var c = 25;
+// var d = 50;
+// var e = a-b+c*d;
+// alert(e);
+
+// var a = 10;
+// var b = 15;
+// var c = 25;
+// var d = 50;
+// var e = (a+b-c)*d;
+// console.log(e);
+
+
+var a = 10 ;
+var b= a++ + ++a - ++a - a++ + ++a;
+alert(b);
